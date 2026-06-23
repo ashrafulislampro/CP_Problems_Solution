b437294bfd7bb00847c5ed6b6b45b6ec163ad872
ashrafulislampro <ashrafulislam1659@gmail.com>
2026-06-23 09:21:07 +0600

    contest 91

---
+++ contest_91/B - C.cpp
@@ -0,0 +1,84 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+#define ft first
+#define sd second
+#define pb(x) push_back(x)
+#define ph(x) push(x)
+#define pp() pop()
+#define sz() size()
+#define pll pair<ll, ll>
+
+using ll = long long;
+const ll inf = (ll)1e18;
+const ll N = (ll)3e5 + 5;
+const ll mod = (ll)1e9 + 7;
+
+// Ordered Set Declaration
+template <typename T>
+using ordered_set = tree<
+    T,
+    null_type,
+    less<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>;
+
+// find_by_order(x) => return an iterator of k-th smallest element(0-based)
+// order_of_key(x) => return a number of element less than x
+
+#define ASHRAFUL                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0), cout.tie(0);
+
+void solve()
+{
+    int n;
+    cin >> n;
+    
+    if (n % 3 == 0 || n % 4 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        int tmp = n;
+        while (tmp > 4)
+        {
+            tmp -= 4;
+            if (tmp % 3 == 0)
+            {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+        tmp = n;
+        while (tmp > 3)
+        {
+            tmp -= 3;
+            if (tmp % 4 == 0)
+            {
+                cout << "YES" << endl;
+                return;
+            }
+        }
+        cout << "NO" << endl;
+    }
+}
+int main()
+{
+    ASHRAFUL
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
+// Coded by Ashraful Islam @ml.ashraful37
---
+++ contest_91/D - D.cpp
@@ -0,0 +1,60 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+#define ft first
+#define sd second
+#define pb(x) push_back(x)
+#define ph(x) push(x)
+#define pp() pop()
+#define sz() size()
+#define pll pair<ll, ll>
+
+using ll = long long;
+const ll inf = (ll)1e18;
+const ll N = (ll)3e5 + 5;
+const ll mod = (ll)1e9 + 7;
+
+// Ordered Set Declaration
+template <typename T>
+using ordered_set = tree<
+    T,
+    null_type,
+    less<T>,
+    rb_tree_tag,
+    tree_order_statistics_node_update>;
+
+// find_by_order(x) => return an iterator of k-th smallest element(0-based)
+// order_of_key(x) => return a number of element less than x
+
+#define ASHRAFUL                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0), cout.tie(0);
+
+ordered_set<int> ost;
+void solve()
+{
+    int n;
+    cin >> n;
+
+    if ((n % 4 != 0) || (n % 100 == 0 and n % 400 != 0))
+    {
+        cout << 365 << endl;
+    }
+    else if ((n % 400 == 0) || (n % 4 == 0 and n % 100 != 0) )
+    {
+        cout << 366 << endl;
+    }
+}
+int main()
+{
+    ASHRAFUL
+
+    solve();
+
+    return 0;
+}
+// Coded by Ashraful Islam @ml.ashraful37
