632a1635c26e850018793475a26e6549275faebe
ashrafulislampro <ashrafulislam1659@gmail.com>
2026-08-16 12:35:05 +0600

    the 67th permutation problem

---
+++ 800_Rating_PB/C_The_67_th_Permutation_Problem.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int k = 3 * n;
+
+        for (int i = 1; i <= n; i++) {
+            cout << i << " " << k - 1 << " " << k << " ";
+            k -= 2;
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
