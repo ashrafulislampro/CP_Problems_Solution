9bc6ba664b7c96dae389c490baf3ac0b99fc4cb9
ashrafulislampro <ashrafulislam1659@gmail.com>
2026-09-04 18:23:14 +0600

    Help goblins

---
+++ 900_Rating_PB/B_Clockwork.cpp
@@ -0,0 +1,58 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ft first
+#define sd second
+#define pb(x) push_back(x)
+#define ph(x) push(x)
+#define pp() pop()
+#define sz() size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const ll inf = (ll)1e18;
+const ll N = (ll)3e5 + 5;
+const ll mod = (ll)1e9 + 7;
+
+#define ASHRAFUL                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0), cout.tie(0);
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int left = 1, right = n;
+    vi arr(n + 1);
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (abs(left - i) * 2 >= arr[i] || abs(right - i) * 2 >= arr[i])
+        {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+int main()
+{
+    ASHRAFUL
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
+// Coded by Ashraful Islam @ml.ashraful37
---
+++ CFC_Starter_254/Maximum_Sum.cpp
@@ -0,0 +1,59 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ft first
+#define sd second
+#define pb(x) push_back(x)
+#define ph(x) push(x)
+#define pp() pop()
+#define sz() size()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const ll inf = (ll)1e18;
+const ll N = (ll)3e5 + 5;
+const ll mod = (ll)1e9 + 7;
+
+#define ASHRAFUL                  \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0), cout.tie(0);
+
+// O(N^2) approach
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    k = n - k;
+    vi arr(n + 1), pre_sum(n + 1, 0);
+    for (int i = 1; i <= n; i++)
+        cin >> arr[i];
+
+    // brute-force approach
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        pre_sum[i] = pre_sum[i - 1] + arr[i];
+        if (i >= k)
+            ans = max(ans, (pre_sum[i] - pre_sum[i - k]));
+    }
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ASHRAFUL
+
+    int T = 1;
+    cin >> T;
+    while (T--)
+        solve();
+
+    return 0;
+}
+// Coded by Ashraful Islam @ml.ashraful37
